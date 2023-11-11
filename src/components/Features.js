b28a7a2044303455ec814ac55bd6db1a8ae822c4
calvin <hhaou@outlook.com>
2023-11-11 22:47:25 +0800

    主页显示修改

--- src/components/Features.js
+++ src/components/Features.js
@@ -3,60 +3,255 @@ import '../styles/Features.css';
 
 const Features = () => {
     return (
-
-            <Row gutter={[16, 16]}>
-                <Col>
-                    <Card title="Golang 游戏服务器开发">
-                        <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
-                            Inner Card content
+            <div className="feature" >
+               <h3>求职意向</h3>
+                <Row gutter={[16, 16]}>
+                    <Col>
+                        <Card title="Golang 游戏服务器开发">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
                         </Card>
-                        <Card
-                            style={{
-                                marginTop: 16,
-                            }}
-                            type="inner"
-                            title="Inner Card title"
-                            extra={<a href="#">More</a>}
-                        >
-                            Inner Card content
+                    </Col>
+                    <Col>
+                        <Card title="运维开发">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
                         </Card>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card title="运维开发">
-                        <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
-                            Inner Card content
+                    </Col>
+                    <Col>
+                        <Card title="Web 后端开发">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
                         </Card>
-                        <Card
-                            style={{
-                                marginTop: 16,
-                            }}
-                            type="inner"
-                            title="Inner Card title"
-                            extra={<a href="#">More</a>}
-                        >
-                            Inner Card content
+                    </Col>
+                </Row>
+                <h3>编程软实力</h3>
+                <Row>
+                    <Col>
+                        <Card title="设计模式">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
                         </Card>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card title="Web 后端开发">
-                        <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
-                            Inner Card content
+                    </Col>
+                    <Col>
+                        <Card title="领域驱动设计">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
                         </Card>
-                        <Card
-                            style={{
-                                marginTop: 16,
-                            }}
-                            type="inner"
-                            title="Inner Card title"
-                            extra={<a href="#">More</a>}
-                        >
-                            Inner Card content
+                    </Col>
+                </Row>
+                <h3>技术栈</h3>
+                    <Row>
+                    <Col>
+                        <Card title="C++">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
                         </Card>
-                    </Card>
-                </Col>
-            </Row>
+                    </Col>
+                    <Col>
+                        <Card title="Rust">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card title="Lua">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card title="Docker">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card title="Linux">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card title="AWS">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card title="Chat-GPT">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card title="Redis">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card title="Mongo">
+                            <Card type="inner" title="Inner Card title" extra={<a href="#">More</a>}>
+                                Inner Card content
+                            </Card>
+                            <Card
+                                style={{
+                                    marginTop: 16,
+                                }}
+                                type="inner"
+                                title="Inner Card title"
+                                extra={<a href="#">More</a>}
+                            >
+                                Inner Card content
+                            </Card>
+                        </Card>
+                    </Col>
+                </Row>
+            </div>
 
     );
 };
