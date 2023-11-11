706871ca9640e35106128cc162e3f130635fed59
calvin <hhaou@outlook.com>
2023-11-11 12:19:07 +0800

    home use grid layout

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,15 +3,37 @@ import Navbar from '../components/Navbar'; // 导入导航栏组件
 import Banner from '../components/Banner'; // 导入横幅组件
 import Features from '../components/Features'; // 导入特点组件
 import Testimonials from '../components/Testimonials'; // 导入推荐语组件
-import Footer from '../components/Footer'; // 导入页脚组件
+import Footer from '../components/Footer';
+import {Col, Row} from "antd"; // 导入页脚组件
 const Home = () => {
     return (
         <div>
-            <Navbar /> {/* 导航栏 */}
-            <Banner /> {/* 横幅 */}
-            <Features /> {/* 特点 */}
-            <Testimonials /> {/* 推荐语 */}
-            <Footer /> {/* 页脚 */}
+            <Row>
+                <Col span={24}>
+                    <Navbar /> {/* 导航栏 */}
+                </Col>
+            </Row>
+            <Row >
+                <Col span={24}>
+                    <Banner /> {/* 横幅 */}
+                </Col>
+            </Row>
+            <Row>
+                <Col span={24}>
+                    <Features /> {/* 特点 */}
+                </Col>
+            </Row>
+            <Row>
+                <Col span={24}>
+                    <Testimonials /> {/* 推荐语 */}
+
+                </Col>
+            </Row>
+            <Row>
+                <Col span={24}>
+                    <Footer /> {/* 页脚 */}
+                </Col>
+            </Row>
         </div>
     );
 };
